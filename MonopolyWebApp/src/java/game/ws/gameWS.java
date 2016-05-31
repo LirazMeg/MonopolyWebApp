384b201d034d0ba4c5a05f2d6512a9679c74d888
efrat <efrat@EfratPeled>
2016-05-31 18:23:15 +0300

    Merge origin/master
    Conflicts:
    	Quadcopte_Parking_Patrol/src/java/db/DbManager.java

--- MonopolyWebApp/src/java/game/ws/gameWS.java
+++ MonopolyWebApp/src/java/game/ws/gameWS.java
@@ -30,13 +30,11 @@ public class gameWS {
     }
 
     public void createGame(int computerizedPlayers, int humanPlayers, java.lang.String name) throws ws.monopoly.DuplicateGameName_Exception, ws.monopoly.InvalidParameters_Exception {
-        //TODO implement this method
-        throw new UnsupportedOperationException("Not implemented yet.");
+        this.manager.createGame(computerizedPlayers, humanPlayers, name);
     }
 
     public ws.monopoly.GameDetails getGameDetails(java.lang.String gameName) throws ws.monopoly.GameDoesNotExists_Exception {
-        //TODO implement this method
-        throw new UnsupportedOperationException("Not implemented yet.");
+        return this.manager.getGameDetails(gameName);
     }
 
     public java.util.List<java.lang.String> getWaitingGames() {
@@ -44,28 +42,23 @@ public class gameWS {
     }
 
     public int joinGame(java.lang.String gameName, java.lang.String playerName) throws ws.monopoly.GameDoesNotExists_Exception, ws.monopoly.InvalidParameters_Exception {
-        //TODO implement this method
-        throw new UnsupportedOperationException("Not implemented yet.");
+       return this.manager.joinGame(gameName, playerName);
     }
 
     public ws.monopoly.PlayerDetails getPlayerDetails(int playerId) throws ws.monopoly.InvalidParameters_Exception, ws.monopoly.GameDoesNotExists_Exception {
-        //TODO implement this method
-        throw new UnsupportedOperationException("Not implemented yet.");
+        return this.manager.getPlayerDetails(playerId);
     }
 
     public void buy(int arg0, int arg1, boolean arg2) {
-        //TODO implement this method
-        throw new UnsupportedOperationException("Not implemented yet.");
+        this.manager.buy(arg0, arg1, arg2);
     }
 
     public void resign(int playerId) throws ws.monopoly.InvalidParameters_Exception {
-        //TODO implement this method
-        throw new UnsupportedOperationException("Not implemented yet.");
+        this.manager.resign(playerId);
     }
 
     public java.util.List<ws.monopoly.PlayerDetails> getPlayersDetails(java.lang.String gameName) throws ws.monopoly.GameDoesNotExists_Exception {
-        //TODO implement this method
-        throw new UnsupportedOperationException("Not implemented yet.");
+        return this.manager.getPlayersDetails(gameName);
     }
 
 }
