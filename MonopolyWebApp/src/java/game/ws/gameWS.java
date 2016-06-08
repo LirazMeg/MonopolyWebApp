549b1da917c8d02c7aa5c9e04bbcb1abbec6a8c9
Liraz <Liraz@Liraz-PC>
2016-06-08 17:21:08 +0300

    Merge origin/master
    Conflicts:
    	MonopolyFXClient/src/MonopolyFX/MonopolyGameApp.java

--- MonopolyWebApp/src/java/game/ws/gameWS.java
+++ MonopolyWebApp/src/java/game/ws/gameWS.java
@@ -9,6 +9,9 @@ import game.manager.GamesManagerWS;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.jws.WebService;
+import ws.monopoly.DuplicateGameName_Exception;
+import ws.monopoly.GameDoesNotExists_Exception;
+import ws.monopoly.InvalidParameters_Exception;
 
 /**
  *
@@ -17,55 +20,55 @@ import javax.jws.WebService;
 @WebService(serviceName = "MonopolyWebServiceService", portName = "MonopolyWebServicePort", endpointInterface = "ws.monopoly.MonopolyWebService", targetNamespace = "http://monopoly.ws/", wsdlLocation = "WEB-INF/wsdl/gameWS/MonopolyWebServiceService.wsdl")
 public class gameWS {
 
-    private GamesManagerWS manger = new GamesManagerWS();
+    GamesManagerWS manager = new GamesManagerWS();
 
-    public java.util.List<ws.monopoly.Event> getEvents(int eventId, int playerId) throws ws.monopoly.InvalidParameters_Exception {
-        return this.manger.getEvents(eventId, playerId);
+    public java.util.List<ws.monopoly.Event> getEvents(int eventId, int playerId) throws InvalidParameters_Exception {
+        return this.manager.getEvents(eventId, playerId);
     }
 
     public java.util.List<ws.monopoly.PlayerDetails> getPlayersDetails(java.lang.String gameName) throws ws.monopoly.GameDoesNotExists_Exception {
-        return this.manger.getPlayersDetails(gameName);
+        return this.manager.getPlayersDetails(gameName);
     }
 
     public java.util.List<java.lang.String> getWaitingGames() {
-        return this.manger.getWaitingGames();
+        return this.manager.getWaitingGames();
     }
 
     public void createGame(int computerizedPlayers, int humanPlayers, java.lang.String name) throws ws.monopoly.InvalidParameters_Exception, ws.monopoly.DuplicateGameName_Exception {
-        this.manger.createGame(computerizedPlayers, humanPlayers, name);
+        this.manager.createGame(computerizedPlayers, humanPlayers, name);
     }
 
     public void resign(int playerId) throws ws.monopoly.InvalidParameters_Exception {
         try {
-            this.manger.resign(playerId);
+            this.manager.resign(playerId);
         } catch (Exception ex) {
             Logger.getLogger(gameWS.class.getName()).log(Level.SEVERE, null, ex);
         }
-       
+
     }
 
     public int joinGame(java.lang.String gameName, java.lang.String playerName) throws ws.monopoly.GameDoesNotExists_Exception, ws.monopoly.InvalidParameters_Exception {
-        return this.manger.joinGame(gameName, playerName);
+        return this.manager.joinGame(gameName, playerName);
     }
 
     public java.lang.String getBoardXML() {
-        return this.manger.getBoardXML();
+        return this.manager.getBoardXML();
     }
 
     public ws.monopoly.PlayerDetails getPlayerDetails(int playerId) throws ws.monopoly.GameDoesNotExists_Exception, ws.monopoly.InvalidParameters_Exception {
-        return this.getPlayerDetails(playerId);
+        return this.manager.getPlayerDetails(playerId);
     }
 
     public java.lang.String getBoardSchema() {
-        return this.manger.getBoardSchema();
+        return this.manager.getBoardSchema();
     }
 
     public ws.monopoly.GameDetails getGameDetails(java.lang.String gameName) throws ws.monopoly.GameDoesNotExists_Exception {
-        return this.manger.getGameDetails(gameName);
+        return this.manager.getGameDetails(gameName);
     }
 
     public void buy(int arg0, int arg1, boolean arg2) throws ws.monopoly.InvalidParameters_Exception {
-        this.manger.buy(arg1, arg1, arg2);
+        this.manager.buy(arg1, arg1, arg2);
     }
 
 }
