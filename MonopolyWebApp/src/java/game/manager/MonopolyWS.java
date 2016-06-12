386b938d4c739879fd8e517e76b31d9cec868f83
Liraz <Liraz@Liraz-PC>
2016-06-12 21:41:33 +0300

    Merge origin/master
    Conflicts:
    	MonopolyFXClient/src/Scene/JoinScene/JoinGameController.java

--- MonopolyWebApp/src/java/game/manager/MonopolyWS.java
+++ MonopolyWebApp/src/java/game/manager/MonopolyWS.java
@@ -176,7 +176,7 @@ class MonopolyWS {
     }
 
     private void actionMethod() throws Exception {
-        spesificGame.getCurrentPlayer().setResign(true);
+        this.spesificGame.getCurrentPlayer().setResign(true);
         removePlayerThatResignFromList();
         // sendToClient("Clear");
     }
@@ -189,6 +189,7 @@ class MonopolyWS {
         } else {// in case of active game
             addEvents(EventType.PLAYER_RESIGNED, this.spesificGame.getCurrentPlayer().getName(), ZERO);
             this.spesificGame.nextPlayerTurn();
+            addEvents(EventType.PLAYER_TURN, this.spesificGame.getCurrentPlayer().getName(), ZERO);
             if (!this.spesificGame.checkIfIsGameOver()) { // while the game is going - more ten one player
                 addEvents(EventType.GAME_WINNER, this.spesificGame.getWinnerName(), ZERO);
                 //        addEvents(EventType.GAME_OVER, this.spesificGame.getCurrentPlayer().getName(), ZERO);
@@ -196,17 +197,6 @@ class MonopolyWS {
                 doIterion();
 
             }
-
-//            if (doComputerIterations()) {
-//                addEvents(EventType.GAME_WINNER, ZERO, spesificGame.getWinnerName());
-//                addEvents(EventType.GAME_OVER, ZERO, spesificGame.getPlayer(lastIndex));
-//            } else {
-//                spesificGame.removeResignFromList();
-//                if (currentPlayerNumber != Boots.ZERO) {
-//                    currentPlayerNumber = (lastIndex) % spesificGame.getPlayersListSize();
-//                }
-//            }
-            //     spesificGame.clearSavedReturnTiles();
         }
 
     }
